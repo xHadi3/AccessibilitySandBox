831408fc55c96921305c2df306704231dd29c5eb
xHadi3 <hadiessam9@gmail.com>
2025-04-06 00:28:52 +0300

    Initial Commit

---
+++ AccessibilitySandBox/AccessibilitySandBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandBoxApp.swift
+//  AccessibilitySandBox
+//
+//  Created by Hadi Al zayer on 08/10/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessibilitySandBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AccessibilitySandBox
+//
+//  Created by Hadi Al zayer on 08/10/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
